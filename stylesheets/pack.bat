ff0d4b68d616f814519d686fd5643f4ad605e1d7
XhmikosR <xhmikosr@users.sourceforge.net>
2012-09-01 23:54:31 +0300

    remove the minified css files since pack.css is used

--- stylesheets/pack.bat
+++ stylesheets/pack.bat
@@ -6,7 +6,9 @@ SET PATH=C:\MSYS\bin;%PATH%
 ECHO Minifying and combining css files...
 
 cat print.css pygment_trac.css stylesheet.css | cleancss -o pack.css
-cleancss -o print.min.css print.css && cleancss -o pygment_trac.min.css pygment_trac.css && cleancss -o stylesheet.min.css stylesheet.css
+rem cleancss -o print.min.css print.css && ^
+rem cleancss -o pygment_trac.min.css pygment_trac.css && ^
+rem cleancss -o stylesheet.min.css stylesheet.css
 
 ENDLOCAL
 PAUSE
